ae220a75ec04c738cdc0a6d8660e0a6c3fdf421a
bhavasagar <56720922+bhavasagar@users.noreply.github.com>
2020-09-25 13:11:23 +0530

    Update development.py

--- casinos/settings/development.py
+++ casinos/settings/development.py
@@ -2,23 +2,23 @@ from .base import *
 import os
 
 DEBUG = False
-ALLOWED_HOSTS = ['nakshatra.fun','176.9.233.36']
+ALLOWED_HOSTS = ['nakshatra.fun','*']
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'naks',
-        'USER': 'naks',
-        'PASSWORD': 'password',
+        'NAME': '**',
+        'USER': '**',
+        'PASSWORD': '**',
         'HOST': 'localhost',
         'PORT': '',
     }
 }
 
 
-PAYTM_MERCHANT_ID = 'UKwuJe94138276969334'
-PAYTM_SECRET_KEY = 'gQ1L@5IwMhONe%3r'
+PAYTM_MERCHANT_ID = '***'
+PAYTM_SECRET_KEY = '***'
 PAYTM_WEBSITE = 'DEFAULT'
 PAYTM_CHANNEL_ID = 'WEB'
 PAYTM_INDUSTRY_TYPE_ID = 'Retail'
