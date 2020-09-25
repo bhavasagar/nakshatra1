db40391bb9c17bd7c2b7d019ea71d9a0069d0e32
bhavasagar <56720922+bhavasagar@users.noreply.github.com>
2020-09-25 13:12:55 +0530

    Update production.py

--- casinos/settings/production.py
+++ casinos/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 # from decouple import config
 
 DEBUG = config('DEBUG', cast=bool)
-ALLOWED_HOSTS = ['nakshatra.fun','176.9.233.36']
+ALLOWED_HOSTS = ['nakshatra.fun','**']
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
@@ -14,16 +14,16 @@ AUTH_PASSWORD_VALIDATORS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'naks',
-        'USER': 'naks',
-        'PASSWORD': 'password',
+        'NAME': '**',
+        'USER': '**',
+        'PASSWORD': '**',
         'HOST': 'localhost',
         'PORT': '',
     }
 }
 
-PAYTM_MERCHANT_ID = 'UKwuJe94138276969334'
-PAYTM_SECRET_KEY = 'gQ1L@5IwMhONe%3r'
+PAYTM_MERCHANT_ID = '**'
+PAYTM_SECRET_KEY = '**'
 PAYTM_WEBSITE = 'DEFAULT'
 PAYTM_CHANNEL_ID = 'WEB'
 PAYTM_INDUSTRY_TYPE_ID = 'Retail'
